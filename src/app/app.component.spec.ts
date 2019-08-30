4f019c073bc2f72adc4b805eafc2fa25d36a7463
adriana cortes <adrianm-cortesv@unilibre.edu.co>
2019-08-30 10:52:47 -0500

    modificaciones

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'parcial2808-AdrianaCortes'`, () => {
+  it(`should have as title 'Parcial2808'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('parcial2808-AdrianaCortes');
+    expect(app.title).toEqual('Parcial2808');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to parcial2808-AdrianaCortes!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Parcial2808!');
   });
 });
